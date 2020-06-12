9b56d3826db47275d4440019e2eb0f471e7b03c3
nmcy2005 <66370151+nmcy2005@users.noreply.github.com>
2020-06-12 09:59:27 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://nmcy2005:Kimlo2005@cluster0-hmbcp.mongodb.net/mern_stack_shoppinglist?retryWrites=true&w=majority",
+    "mongodb+srv://nmcy2005:<Password>@cluster0-hmbcp.mongodb.net/mern_stack_shoppinglist?retryWrites=true&w=majority",
 };
